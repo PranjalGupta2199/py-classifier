cc98180715437cde18584163f87e1ea04b2e103e
Pranjal Gupta <pranjalgupta2199@gmail.com>
2020-04-27 20:14:23 +0530

    assn2: Fixes error and dry tests neural net

--- Assignment-2/neural_net.py
+++ Assignment-2/neural_net.py
@@ -1,9 +1,12 @@
-from sklearn.model_selection import train_test_split
+from abc import ABC, abstractmethod
+import pickle
+from itertools import tee, islice, chain
+
+import pandas as pd
 import numpy as np
 from numpy.random import uniform, random, normal
-from itertools import tee, islice, chain
-import pickle
-from abc import ABC, abstractmethod
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 class ActivationFunction(ABC):
@@ -27,8 +30,9 @@ class Tanh(ActivationFunction):
         self.prev = w
         return np.tanh(w)
 
-    def backward(self, dA, z):
-        pass
+    def backward(self):
+        val = self.forward(self.prev)
+        return 1.0 - np.tanh(val) ** 2
 
 class Sigmoid(ActivationFunction):
 
@@ -54,10 +58,13 @@ class Softmax(ActivationFunction):
 class ReLU(ActivationFunction):
 
     def forward(self, w):
+        self.prev = w
         return np.maximum(0, w)
 
-    def backward(self, dA, w):
-        pass
+    def backward(self):
+        self.prev[self.prev <= 0] = 0
+        self.prev[self.prev > 0] = 1
+        return self.prev
 
 def get_actv_fn(fn):
     fns = {
@@ -80,7 +87,7 @@ class Initializer:
 
     @staticmethod
     def get(fn, shape, a=-1, b=1):
-        if fn in ('one', 'zero'):
+        if fn in ('one', 'zero', 'random'):
             return Initializer.init[fn](shape)
         else:
             return Initializer.init[fn](a, b, shape)
@@ -94,8 +101,8 @@ class Layer:
         self._init_weight(init_method, activation_fn)
 
     def _init_weight(self, init_method, activation_fn):
-        self.w = Initializer.get(init_method, (self.output_dim, self.input_dim))
-        self.b = Initializer.get(init_method, (self.output_dim, 1))
+        self.w = Initializer.get(init_method, (self.output_dim, self.input_dim), 0, 1)
+        self.b = np.ones((1, self.output_dim))
         self.activation_fn = get_actv_fn(activation_fn)
 
     def __call__(self, *args, **kwargs):
@@ -103,25 +110,26 @@ class Layer:
 
     def forward(self, X):
         batch_size, _ = X.shape
-        self.a = self.activation_fn(np.dot(self.w, X) + self.b)
-        # print ("\toutput {}".format(self.a.shape))
+        self.a = self.activation_fn(np.dot(self.w, X.T).reshape(batch_size, -1) +
+                                           self.b)
         return self.a
 
     def backward(self, a_prev, grad_, lamda):
         fn_grad = self.activation_fn.backward()
         # print ("\t\tFunction Gradient {}".format(fn_grad.shape))
-        # print ("\t\tw update {}".format(np.dot(grad_, a_prev.T).shape))
+        # print ("\t\tw update {}".format(np.dot(grad_.T, a_prev).shape))
         grad_ = grad_ * fn_grad
 
-        self.w -= lamda * np.dot(grad_, a_prev.T)
-        self.b -= lamda * np.sum(grad_, axis=1, keepdims=True)
+        self.w = self.w - lamda * np.dot(grad_.T, a_prev)
+        self.b = self.b - lamda * np.sum(grad_, axis=0, keepdims=True)
 
-        return np.dot(self.w.T, grad_)
+        return np.dot(grad_, self.w)
 
     def __str__(self, indent='\t'):
         string = ''
         string += "({})\n".format(__class__.__name__)
         string += indent * 2 + "Weight : {}\n".format(self.w.shape)
+        string += indent * 2 + "Output dim : {}\n".format(self.output_dim)
         string += indent * 2 + "Bias : {}\n".format(self.b.shape)
         string += indent * 2 + "Activation : {}\n".format(self.activation_fn.__class__.__name__)
         return string
@@ -129,11 +137,8 @@ class Layer:
 
 class NeuralNet:
 
-    def __init__(self, learning_rate, layer_dims, actn_fn,
-                 n_epoch, n_init, initializer):
+    def __init__(self, learning_rate, layer_dims, actn_fn, initializer):
         super().__init__()
-        self.n_init = n_init
-        self.n_epoch = n_epoch
         self.initializer = initializer
         self.lamda = learning_rate
         self.init_model_params(layer_dims, actn_fn)
@@ -147,7 +152,7 @@ class NeuralNet:
                 input_dim=i,
                 output_dim=j,
                 activation_fn=fn,
-                init_method='one',
+                init_method=self.initializer,
             )
             for i,j,fn in zip(prev_,
                               next_,
@@ -160,9 +165,9 @@ class NeuralNet:
             return pickle.load(f)
 
     def __call__(self, *args, **kwargs):
-        return self._forward(*args, **kwargs)
+        return self.forward(*args, **kwargs)
 
-    def _forward(self, X):
+    def forward(self, X):
         # print ("Forward Propagation :")
         for layer in self.layers:
             # print ("\tinput {}".format(X.shape))
@@ -172,7 +177,7 @@ class NeuralNet:
 
     def backward(self, X, y_true, y_pred):
         # print ("Back Propagation :")
-        grad = 2 * (y_true - y_pred)
+        grad = -2.0 * (y_true - y_pred)
         curr, prev = reversed(self.layers), reversed(self.layers)
         prev = islice(prev, 1, None)
 
@@ -196,16 +201,29 @@ class NeuralNet:
         return self.settings
 
 
-if __name__ == "__main__":
+def train():
     n = NeuralNet(
             learning_rate=0.1,
-            layer_dims=[4, 300, 2, 123, 234, 1],
-            actn_fn=["sigmoid"] * 6,
-            n_epoch=10,
-            n_init=1,
-            initializer='one'
+            layer_dims=[10, 1],
+            actn_fn=["sigmoid"] * 4 ,
+            initializer='uniform'
     )
-    x = 2.0 * np.ones((4,2))
-    y_pred = n(x)
-    n.backward(x, np.ones((1,2)), y_pred)
-    print (n)
+    data = pd.read_csv('housepricedata.csv')
+    headers = data.columns
+    X = StandardScaler().fit_transform(data[headers[:-1]])[:100, :]
+    Y = data[headers[-1]].to_numpy().reshape(-1, 1)[:100, :]
+
+    n_epoch = 50000
+    print_batch = 1000
+    for n_iter in range(n_epoch):
+        y_pred = n(X)
+        # print (y_pred.shape)
+        n.backward(X, Y, y_pred)
+        if n_iter % print_batch == 0:
+            print (np.sum(np.square(Y - y_pred)))
+
+    for j, k in zip(Y,n(X)):
+        print (j, k)
+
+if __name__ == "__main__":
+    train()
